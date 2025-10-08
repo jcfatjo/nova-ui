ea66b98d2eac7504220313ca867a33a5eb5996d6
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-08 21:21:58 +0200

    refactor(link): integrate BaseColorComponent and streamline variant styling

--- src/components/Typography/Link/Link.component.ts
+++ src/components/Typography/Link/Link.component.ts
@@ -1,10 +1,49 @@
-import { BaseComponent } from "../../Base.component.ts";
+import { BaseColorComponent } from "../../Base/Color/BaseColor.component.ts";
+import type { Variant } from "./Link.types.ts";
 
-export class LinkComponent extends BaseComponent {
+export class LinkComponent extends BaseColorComponent {
   public static is = "link-component";
+
+  public static override get observedAttributes() {
+    return [...BaseColorComponent.observedAttributes, "variant"];
+  }
+
+  private _variant: Variant = "plain";
+
+  public get variant() {
+    return this._variant;
+  }
+
+  public set variant(value: Variant) {
+    this._variant = value;
+  }
+
+  public attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    if (name === "variant") {
+      this.variant = (newValue as Variant) || "plain";
+    } else {
+      super.attributeChangedCallback(name, oldValue, newValue);
+    }
+  }
 
   public connectedCallback() {
     super.connectedCallback();
+    this._initStyles();
+  }
+
+  private _initStyles() {
+    const styleDeclaration = {
+      selector: `.variant-${this.variant}.color-palette-${this.colorPalette} a`,
+      rules: [
+        { property: "--mix-textDecorationColor", value: `color-mix(in srgb, currentColor 20%, transparent)` },
+      ],
+    };
+
+    this.addStyleDeclaration(styleDeclaration);
   }
 }
 
--- src/components/Typography/Link/Link.types.ts
+++ src/components/Typography/Link/Link.types.ts
@@ -1,7 +1,7 @@
 import type { HTMLAttributes } from "astro/types";
 import type { BaseProps, ColorProps } from "../../Components.types.ts";
 
-type Variant = "plain" | "underline";
+export type Variant = "plain" | "underline";
 
 type LinkBaseProps = {
   variant?: Variant;
