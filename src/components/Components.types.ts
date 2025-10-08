159c350c547dd0cebe62c7a643f2f16f2a586560
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-08 09:26:40 +0200

    feat: introduce comprehensive color system variables

--- src/components/Components.types.ts
+++ src/components/Components.types.ts
@@ -1,31 +1,15 @@
+import colors from "tailwindcss/colors";
+
 export type BaseProps = {
   as?: astroHTML.JSX.Element;
   asChild?: boolean;
 };
 
-type ColorPalette =
-  "red"
-  | "orange"
-  | "amber"
-  | "yellow"
-  | "lime"
-  | "green"
-  | "emerald"
-  | "teal"
-  | "cyan"
-  | "sky"
-  | "blue"
-  | "indigo"
-  | "violet"
-  | "purple"
-  | "fuchsia"
-  | "pink"
-  | "rose"
-  | "slate"
-  | "gray"
-  | "zinc"
-  | "neutral"
-  | "stone";
+type NestedColorKeys<T> = {
+  [K in keyof T]: T[K] extends object ? K : never
+}[keyof T];
+
+export type ColorPalette = NestedColorKeys<typeof colors>;
 
 export type ColorProps = {
   colorPalette?: ColorPalette;
