fa1a569fe484347d38dac5e49b40037d6b405e0c
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-08 23:58:23 +0200

    refactor(components): remove unused constant

--- src/components/Components.constants.ts
+++ src/components/Components.constants.ts
@@ -1,11 +1,4 @@
-import colors from "tailwindcss/colors";
-import type { ColorPalette } from "./Components.types.ts";
-
 export const DISABLED_CLASS_LIST = [
   "disabled:opacity-50",
   "disabled:cursor-not-allowed",
 ];
-
-export const COLORS: ColorPalette[] = Object.entries(colors)
-.filter(([_, value]) => typeof value === "object" && value !== null)
-.map(([name]) => name as ColorPalette);
