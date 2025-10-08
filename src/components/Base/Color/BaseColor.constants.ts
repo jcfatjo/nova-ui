cdb26fc4240e0aacc878f94a21883adf1f0b9116
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-08 22:48:35 +0200

    feat(colors): migrate color palette management to static CSS classes

--- src/components/Base/Color/BaseColor.constants.ts
+++
@@ -1,1 +0,0 @@
-export const COMPONENTS_STYLES_ID = "components-styles";
--- src/components/Base/Color/BaseColor.types.ts
+++
@@ -1,9 +0,0 @@
-export type RuleDeclaration = {
-  property: string;
-  value: string;
-}
-
-export type StyleDeclaration = {
-  selector: string;
-  rules: RuleDeclaration[];
-};
