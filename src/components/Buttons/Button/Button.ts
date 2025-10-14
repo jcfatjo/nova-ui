a2c68b13557def18b430f334d5bc92656c14d330
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-14 18:58:56 +0200

    refactor(button): decouple disabled state logic into utility function

--- src/components/Buttons/Button/Button.ts
+++ src/components/Buttons/Button/Button.ts
@@ -3,3 +3,7 @@ import type { ButtonProps } from "./Button.types.ts";
 export function getClassList({ size = "md", variant = "solid", colorPalette = "gray" }: ButtonProps) {
   return `nova-button size-${size} variant-${variant} color-palette-${colorPalette}`;
 }
+
+export function getDisabledState({ disabled, loading }: ButtonProps) {
+  return disabled || loading || undefined;
+}
--- src/components/Typography/Link/Link.types.ts
+++ src/components/Typography/Link/Link.types.ts
@@ -4,7 +4,9 @@ import type { BaseProps, ColorProps } from "../../Components.types.ts";
 export type Variant = "plain" | "underline";
 
 type LinkBaseProps = {
+  href: string;
   variant?: Variant;
+  isExternal?: boolean;
 } & BaseProps & ColorProps;
 
 export type LinkProps = LinkBaseProps & HTMLAttributes<"a">;
