348f4db57875006988103a7cf689f0b3d6f39396
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-15 21:50:30 +0200

    refactor(buttons): general code cleanup and improvements

--- src/components/Buttons/Button/Button.ts
+++ src/components/Buttons/Button/Button.ts
@@ -1,5 +1,5 @@
-import type { ButtonsBaseProps } from "../Buttons.types.ts";
+import type { ButtonProps } from "./Button.types.ts";
 
-export function getClassList({ size = "md", variant = "solid", colorPalette = "gray" }: Partial<ButtonsBaseProps>) {
+export function getButtonClassList({ size = "md", variant = "solid", colorPalette = "gray" }: ButtonProps) {
   return `nova-button size-${size} variant-${variant} color-palette-${colorPalette}`;
 }
