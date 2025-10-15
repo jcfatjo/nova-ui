b67545afbac95914ae97c2cf8d7ea3fe0217c64d
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-15 13:30:16 +0200

    refactor(button): update component for new prop types and cleanup

--- src/components/Buttons/Button/Button.ts
+++ src/components/Buttons/Button/Button.ts
@@ -1,9 +1,5 @@
-import type { ButtonProps } from "./Button.types.ts";
+import type { ButtonsBaseProps } from "../Buttons.types.ts";
 
-export function getClassList({ size = "md", variant = "solid", colorPalette = "gray" }: ButtonProps) {
+export function getClassList({ size = "md", variant = "solid", colorPalette = "gray" }: Partial<ButtonsBaseProps>) {
   return `nova-button size-${size} variant-${variant} color-palette-${colorPalette}`;
-}
-
-export function getDisabledState({ disabled, loading }: ButtonProps) {
-  return disabled || loading || undefined;
 }
