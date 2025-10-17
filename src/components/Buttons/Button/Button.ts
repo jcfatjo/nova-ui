aaabbc21318bd281ae80b3048d32a556e63ca97f
José Carlos Fatjó García <jc.fatjo@outlook.com>
2025-10-17 16:48:20 +0200

    feat: refactor Button component logic to use new styling and loading props

--- src/components/Buttons/Button/Button.ts
+++ src/components/Buttons/Button/Button.ts
@@ -1,10 +1,25 @@
 import type { ButtonProps } from "./Button.types.ts";
 
-export function getButtonClassList({ size = "md", variant = "solid", colorPalette = "gray" }: ButtonProps) {
-  return [
+export function getButtonClassList(
+  {
+    shape = "rounded",
+    fill = "solid",
+    size = "md",
+    colorPalette = "amber",
+    iconOnly,
+  }: ButtonProps,
+) {
+  const buttonClassList = [
     "nova-button",
+    `shape-${shape}`,
+    `fill-${fill}`,
     `size-${size}`,
-    `variant-${variant}`,
     `color-palette-${colorPalette}`,
   ];
+
+  if (iconOnly) {
+    buttonClassList.push("icon-only");
+  }
+
+  return buttonClassList;
 }
